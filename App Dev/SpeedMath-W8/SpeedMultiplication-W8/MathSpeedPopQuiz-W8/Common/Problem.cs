df9c72f5808b11f34a6571562907c669a3500277
Viquoc Quan <vtquan@live.com>
2013-09-25 21:27:53 -0400

    Move math quiz into its own folder

---
+++ App Dev/SpeedMath-W8/SpeedMultiplication-W8/MathSpeedPopQuiz-W8/Common/Problem.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MathSpeedPopQuiz_W8.Common
+{
+    class Problem
+    {
+        public String prob { get; set; }
+
+        public String ans { get; set; }
+
+        public String input { get; set; }
+
+        public bool correct { get; set; }
+
+        public Problem(int num1, int num2)
+        {
+            prob = num1 + " + " + num2;
+
+            ans = (num1 + num2).ToString();
+        }
+    }
+}
---
+++ App Dev/SpeedMath-W8/SpeedSubtraction-W8/MathSpeedPopQuiz-W8/Common/Converter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Media;
+
+
+namespace MathSpeedPopQuiz_W8.Common
+{
+    class BoolToBrushConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, string language)
+        {
+            bool boolValue = (bool)value;
+            if (boolValue)
+                return new SolidColorBrush(Colors.Green);
+            return new SolidColorBrush(Colors.Red);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, string language)
+        {
+            SolidColorBrush brushValue = value as SolidColorBrush;
+            if (brushValue == new SolidColorBrush(Colors.Red))
+                return 1;
+            return 0;
+        }
+    }
+}
